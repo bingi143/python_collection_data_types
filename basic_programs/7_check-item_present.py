bea99865fcd0c015fcaaf70cf573ec4fe14cba1f
bingi143 <bingivenkateswarlu578@gmail.com>
2024-08-19 11:00:32 +0530

    [Venkatesh] Add: python program check item present or not

---
+++ basic_programs/7_check-item_present.py
@@ -0,0 +1,32 @@
+'''
+
+@Author: Venkatesh
+@Date: 2024-08-17 18:00:30
+@Last Modified by: Venkatesh
+@Last Modified time: 2024-08-16 18:00:30
+@Title : Python Program is check given item present or not
+
+'''
+
+
+def check_method(list_):
+    '''
+          Description: 
+                this function is check given item present or not
+          Parameters: 
+                 list_: list of elelments
+          Return : 
+                this function returnting days
+        '''
+    print(3 in list_)
+    print(-1 in list_)
+
+   
+def main():
+    list_=[1,5,8,3]
+    check_method(list_)
+
+
+if __name__=="__main__":
+    main()
+
